965b902fe452ccdf15896bd7cde8185cc1c679c7
Renkigouda <renkigoudaharalakatti09@gmail.com>
2026-02-10 19:43:02 +0530

    Frontend environment setup and services added

---
+++ frontend/src/app/services/product.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = `${environment.apiUrl}/api/products`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getProductById(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createProduct(product: any): Observable<any> {
+    return this.http.post(this.apiUrl, product);
+  }
+
+  updateProduct(id: number, product: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
